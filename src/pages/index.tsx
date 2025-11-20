ebfd32f2a6c68478cc4fb36acd925e13c69378ff
noufalrahim <noufalrahim6784@gmail.com>
2025-11-20 09:52:16 +0530

    fix: fixed build issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import { MainLayout } from "@/layout";
-import { HomePage } from "./home";
 
 export default function Home() {
   return (
     <div>
       <MainLayout>
-        <HomePage />
+        <div></div>
       </MainLayout>
     </div>
   );
